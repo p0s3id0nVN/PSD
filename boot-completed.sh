88a00ecc31ad4ae911534ddc7be3794b32ba00f4
p0s3id0n <trocngo86@gmail.com>
2026-04-11 20:13:08 +0700

    Update boot-completed.sh

--- boot-completed.sh
+++ boot-completed.sh
@@ -1,7 +1,7 @@
 MODDIR=${0%/*}
 KSU_BIN=/data/adb/ksu/bin/ksud
 SUSFS_BIN=/data/adb/ksu/bin/susfs
-PERSISTENT_DIR=/data/adb/p0s3id0n
+PERSISTENT_DIR=/data/adb/PSD
 # Load config
 [[ -e ${PERSISTENT_DIR}/config.sh ]] && source ${PERSISTENT_DIR}/config.sh
 source ${MODDIR}/utils.sh
